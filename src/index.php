27b32c41ff960ada990480be0faa2ba7647e37ee
Jun Zheng <junzheng@exchange.niyume.com>
2017-01-22 09:11:52 -0500

    Fixed create button

--- src/index.php
+++ src/index.php
@@ -85,7 +85,7 @@
 
             <h1>Learn / Play / Hack</h1>
             <br><br>
-            <a href="#base"><button id="create-your-own">Create Your Own Game</button></a>
+            <a href="#base"><button id="create-your-own"><span class="glyphicon glyphicon-plus"></span> Create Your Own</button></a>
             <div class="showcase-row container">
                 
             </div>
